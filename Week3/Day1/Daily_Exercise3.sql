fe25e566f6b0635fa7a3558d305ef8ece5176a04
Alexander David <al1da2pe3@gmail.com>
2024-08-18 20:05:50 +0200

    added daily exercise

---
+++ Week3/Day1/Daily_Exercise3.sql
@@ -0,0 +1,25 @@
+-- CREATE TABLE actors2(
+--  actor_id SERIAL PRIMARY KEY,
+--  first_name VARCHAR (50) NOT NULL,
+--  last_name VARCHAR (100) NOT NULL,
+--  age DATE NOT NULL,
+--  number_oscars SMALLINT NOT NULL
+-- )
+
+-- INSERT INTO actors2 (first_name, last_name, age, number_oscars)
+-- VALUES('Matt','Damon','08/10/1970', 5);
+
+-- INSERT INTO actors2 (first_name, last_name, age, number_oscars)
+-- VALUES('George','Clooney','06/05/1961', 2);
+
+-- SELECT COUNT (actor_id)
+-- FROM actors2
+
+-- INSERT INTO actors2 (first_name, last_name, age, number_oscars)
+-- VALUES
+-- 	('Pero', 'Sekig', '1986-07-02', 7);
+
+
+INSERT INTO actors2 (first_name, last_name, age, number_oscars)
+VALUES
+	(, 'Sekig', '1986-07-02', 7);
