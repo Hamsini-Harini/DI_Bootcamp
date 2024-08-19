8ede7f7b5fc1dae173e70908932e5fd09c79980c
Alexander David <al1da2pe3@gmail.com>
2024-08-19 20:30:23 +0200

    daily challenge edited

--- Week3/Day2/Daily_Challenge.sql
+++ Week3/Day2/Daily_Challenge.sql
@@ -1,10 +1,10 @@
---
-guessing the outputs
 
-1 - No output or be zero
+-- guessing the outputs
 
-2 -  the query will count 3 rows (ids 6, 7, and NULL) and return 3
+-- 1 - will return IDs 5 ,6 and 7 
 
-3 - no rows will satisfy the NOT IN condition, and the output will be 0.
+-- 2 -  the query will count 3 rows (ids 6, 7, and NULL) and return 3
 
-4 - same as in 2 
+-- 3 - no rows will satisfy the NOT IN condition - you cant comapre anything to NULL, and the output will be 0.
+
+-- 4 - I expect IDs 6 and 7 to be returned and probably NULL as well 
