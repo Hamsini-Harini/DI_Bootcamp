49ebeb29b4ce19ef266cd6c493db3984756f51d8
Alexander David <al1da2pe3@gmail.com>
2024-08-19 20:23:29 +0200

    daily challenge added

---
+++ Week3/Day2/Daily_Challenge.sql
@@ -0,0 +1,10 @@
+--
+guessing the outputs
+
+1 - No output or be zero
+
+2 -  the query will count 3 rows (ids 6, 7, and NULL) and return 3
+
+3 - no rows will satisfy the NOT IN condition, and the output will be 0.
+
+4 - same as in 2 
